e73b4f3b329437d7a468af67e60a95a7cc0acf27
mti <mti@purdue.edu>
2022-02-26 20:20:36 -0500

    design stuff

--- uploadPDF/example/views.py
+++ uploadPDF/example/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from urllib.parse import urlencode
 from django.urls import reverse
 from .forms import companyForm
+import json
 
 # Create your views here.
 def welcome(request):
@@ -17,7 +18,14 @@ def display_report(request):
         form = companyForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['company_name']
-            return render(request, 'administration/display.html', {'name': name})
+
+            sample = {
+                "name": name,
+                "GrossProfit": "60000",
+                "NetIncome": "40000"
+            }
+
+            return render(request, 'administration/display.html', {'sample': sample, 'name': name})
     else:
         form = companyForm()
         return render(request, 'administration/upload.html', {'form': form})
